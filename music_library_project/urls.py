3e2a3970ccc4d90296c0c867d3663919156780c7
mayragann <mayragann@gmail.com>
2022-04-25 12:29:30 -0400

    Added SuperUser Admin to Add Items to Songs Table

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('muisc/', include('songs.urls'))
+    path('music/', include('songs.urls'))
 ]
