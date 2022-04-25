dfb431966b77c2fa09c5b88523db11a56e861438
mayragann <mayragann@gmail.com>
2022-04-25 12:13:07 -0400

    Updated Project URLS + Created First View Function + Created Songs App Url File

---
+++ songs/urls.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlparse
+from django.urls import path
+from . import views
+
+urlpatterns  = [
+    path('', views.songs_list)
+    # path('<int:pk>/', views.songs_detail)
+]
