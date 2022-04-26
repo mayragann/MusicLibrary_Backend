335eb3fe29eb93ca4099185115ec6655c6e7c74a
mayragann <mayragann@gmail.com>
2022-04-26 14:32:06 -0400

    Added Delete Function plus Debugger

--- songs/views.py
+++ songs/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import get_list_or_404, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SongSerializer
 from .models import Song
-from songs import serializers
+
 
 @api_view(['GET', 'POST'])
 def songs_list(request):
     if request.method == 'GET':
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = SongSerializer(data=request.data)
         serializer.is_valid (raise_exception=True)
@@ -25,7 +25,10 @@ def song_detail(request, pk):
         serializer = SongSerializer(song);
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = SongSerializer(song, data=request.data)
+        serializer = SongSerializer(song, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
